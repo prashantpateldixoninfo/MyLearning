26f8e0f080e3b2d7e1522305af4b432c89030ec8
Prashant Patel <prashant.patel@dixoninfo.com>
2023-08-31 17:28:49 +0530

    Change content to container and move icone to right

--- ReactJS_Project/contact-app/src/components/ContactCard.js
+++ ReactJS_Project/contact-app/src/components/ContactCard.js
@@ -2,22 +2,29 @@ import React from "react";
 import avatar from "../images/avatar.jpg";
 
 const ContactCard = (props) => {
-    const { id, name, email } = props.myContact;
-    console.log("My Contact => ", props.myContact);
-    return (
-      <div className="item">
-        <img className="ui avatar image" src={avatar} alt="avatar" />
-        <div className="content">
-          <div className="header">{name}</div>
-          <div>{email}</div>
-        </div>
+  const { id, name, email } = props.myContact;
+  console.log("My Contact => ", props.myContact);
+  return (
+    <div className="item">
+      <img className="ui avatar image" src={avatar} alt="avatar" />
+      <div className="container">
+        <div className="header" >{name}</div>
+        <div>{email}</div>
         <i
           className="trash alternate outline icon"
-          style={{ color: "red", marginTop: "7px" }}
+          style={
+            {
+              color: "green",
+              // marginLeft: "20px",
+              // paddingLeft: "20px",
+              float: "right"
+            }
+          }
           onClick={() => props.clickHandler(id)}
         ></i>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ContactCard;
