cea9e1be657ddb5204c69e55837cf146b9642b78
Prashant Patel <prashant.patel@dixoninfo.com>
2023-09-05 18:00:55 +0530

    Added validation functionality

--- ReactJS_Project/react-form-native/src/App.js
+++ ReactJS_Project/react-form-native/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-  const initialValue = {username: "", email: "", password: ""};
+  const initialValue = { username: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValue);
+  const [formErrors, setFormErrors] = useState({});
 
   // console.log("formValues = ", formValues);
 
@@ -13,22 +14,63 @@ function App() {
 
   function handleChange(e) {
     // console.log("Prashant Patel => ", e.target);
-    const {name, value} =  e.target;
+    const { name, value } = e.target;
     // console.log("name => ", name);
     // console.log("value => ", value);
-    setFormValues({...formValues, [name]: value});
+    setFormValues({ ...formValues, [name]: value });
     // console.log("formValues => ", formValues);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("Submit ==> ", formValues);
+    // Validate the formValues
+    setFormErrors(validate(formValues));
+  }
+
+  function validate(values) {
+    const errors = {};
+    // console.log("validate ==> ", values);
+    const regex_email = /^\S+@\S+\.\S+$/i;
+
+    // Username Validation
+    if (values.username === "") { // Emptyness
+      errors.username = "Username is required";
+      // console.log(errors.username);
+    }
+
+    // Email Validation
+    if (values.email === "") { // Emptyness
+      errors.email = "Email is required";
+      // console.log(errors.email);
+    }
+    else if (!regex_email.test(values.email)) {
+      errors.email = "Email format is not valid";
+    }
+
+
+    // Password Validation
+    if (values.password === "") { // Emptyness
+      errors.password = "Password is required";
+      // console.log(errors.password);
+    }
+    else if (values.password.length < 4 || values.password.length > 10) {
+      errors.password = "Password length should be between 4 and 10";
+    }
+
+    return errors;
   }
 
   return (
     <div className='container'>
-      <form>
+      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit}>
         <h1>Login Form</h1>
         <div className='ui divider'></div>
         <div className='ui form'>
           <div className='field'>
             <label>Username</label>
-            <input 
+            <input
               type="text"
               name="username"
               placeholder='Full Name'
@@ -37,6 +79,7 @@ function App() {
             >
             </input>
           </div>
+          <p>{formErrors.username}</p>
           <div className='field'>
             <label>Email</label>
             <input
@@ -48,9 +91,10 @@ function App() {
             >
             </input>
           </div>
+          <p>{formErrors.email}</p>
           <div className='field'>
             <label>Password</label>
-            <input 
+            <input
               type="password"
               name="password"
               placeholder='Your Password'
@@ -59,6 +103,7 @@ function App() {
             >
             </input>
           </div>
+          <p>{formErrors.password}</p>
           <button className='fluid ui button green'>Submit</button>
         </div>
       </form>
