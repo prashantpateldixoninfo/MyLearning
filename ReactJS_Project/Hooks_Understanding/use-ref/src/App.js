6e03d8ce9493b16ba7cc5c40fef4a43edc35bbf1
Prashant Patel <prashant.patel@dixoninfo.com>
2023-09-15 12:44:00 +0530

    Added small code for useRef()

--- ReactJS_Project/Hooks_Understanding/use-ref/src/App.js
+++ ReactJS_Project/Hooks_Understanding/use-ref/src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import { useEffect, useRef, useState } from "react";
+
+/* DOM Reference */
+/* useRef for storing previous state/value */
 
 function App() {
+
+  const [name, setName] = useState();
+  const [counter, setCounter] = useState(Math.ceil(Math.random() * 100));
+  let inputRef = useRef(null);
+  let previousRandNum = useRef();
+
+  function resetInput() {
+    setName("");
+    inputRef.current.focus();
+  }
+
+  useEffect(() => {
+    previousRandNum.current = counter;
+  }, [counter]);
+
   return (
     <div className="App">
-      <h1>Hello, use-ref :)</h1>
+      <hr></hr>
+      <input
+        ref={inputRef}
+        type="text"
+        name="name"
+        // autoComplete="off"
+        value={name}
+        onChange={(e) => setName(e.target.value)}>
+      </input>
+      <button onClick={resetInput}>Reset</button>
+      <hr></hr>
+      <h1>Random Counter : {counter}</h1>
+      { 
+        typeof previousRandNum.current !== "undefined" && 
+        <h2>Previous Random Num: {previousRandNum.current}</h2>
+      }
+      <button onClick={() => setCounter(Math.ceil(Math.random() * 100))}>Random Generator</button>
+      <hr></hr>
     </div>
   );
 }
