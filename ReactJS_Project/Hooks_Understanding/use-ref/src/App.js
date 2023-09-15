2535c8f15b1fe01fc1a0e35c290f732ce2baec7e
Prashant Patel <prashant.patel@dixoninfo.com>
2023-09-15 15:55:27 +0530

    Added NAV bar and scrolling feature

--- ReactJS_Project/Hooks_Understanding/use-ref/src/App.js
+++ ReactJS_Project/Hooks_Understanding/use-ref/src/App.js
@@ -10,6 +10,9 @@ function App() {
   const [counter, setCounter] = useState(Math.ceil(Math.random() * 100));
   let inputRef = useRef(null);
   let previousRandNum = useRef();
+  const firstCatRef = useRef(null);
+  const secondCatRef = useRef(null);
+  const thirdCatRef = useRef(null);
 
   function resetInput() {
     setName("");
@@ -19,6 +22,30 @@ function App() {
   useEffect(() => {
     previousRandNum.current = counter;
   }, [counter]);
+
+  function handleScrollToFirstCat() {
+    firstCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    });
+  }
+
+  function handleScrollToSecondCat() {
+    secondCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    });
+  }
+
+  function handleScrollToThirdCat() {
+    thirdCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    });
+  }
 
   return (
     <div className="App">
@@ -34,11 +61,44 @@ function App() {
       <button onClick={resetInput}>Reset</button>
       <hr></hr>
       <h1>Random Counter : {counter}</h1>
-      { 
-        typeof previousRandNum.current !== "undefined" && 
+      {
+        typeof previousRandNum.current !== "undefined" &&
         <h2>Previous Random Num: {previousRandNum.current}</h2>
       }
       <button onClick={() => setCounter(Math.ceil(Math.random() * 100))}>Random Generator</button>
+      <hr></hr>
+      <>
+        <nav>
+          <button onClick={handleScrollToFirstCat}>Tom</button>
+          <button onClick={handleScrollToSecondCat}>Maru</button>
+          <button onClick={handleScrollToThirdCat}>Jellylorum</button>
+        </nav>
+        <div>
+          <ul>
+            <li>
+              <img
+                src="https://placekitten.com/g/200/200"
+                alt="Tom"
+                ref={firstCatRef}
+              />
+            </li>
+            <li>
+              <img
+                src="https://placekitten.com/g/300/200"
+                alt="Maru"
+                ref={secondCatRef}
+              />
+            </li>
+            <li>
+              <img
+                src="https://placekitten.com/g/250/200"
+                alt="Jellylorum"
+                ref={thirdCatRef}
+              />
+            </li>
+          </ul>
+        </div>
+      </>
       <hr></hr>
     </div>
   );
