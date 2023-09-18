a61eb50fec1129425265ea93078c1aa61e4bc1e3
Prashant Patel <prashant.patel@dixoninfo.com>
2023-09-18 13:10:24 +0530

    Added useMemo hooks in application

--- ReactJS_Project/Hooks_Understanding/use-memo/src/App.js
+++ ReactJS_Project/Hooks_Understanding/use-memo/src/App.js
@@ -1,11 +1,55 @@
 import './App.css';
+import React, { useState, useMemo } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const [name, setName] = useState("");
+  let result = useMemo(() => {
+    return factorial(counter);
+  }, [counter]);
+
   return (
     <div className="App">
-      <h1>Hello, use-memo :)</h1>
+      <h1>
+        Factorial of {counter} is : <span>{result}</span>
+      </h1>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <hr />
+      <div>
+        <div>
+          <label>Enter Name</label>
+        </div>
+        <input
+          type="text"
+          name="name"
+          placeholder='Enter your name'
+          onChange={(e) => setName(e.target.value)}>
+        </input>
+        <hr></hr>
+        <DisplayName name = {name}/>
+      </div>
     </div>
   );
+}
+
+const DisplayName = React.memo(({name}) => {
+  console.log("name => ", name);
+  return <p>{`My name is ${name}`}</p>  
+})
+
+function factorial(n) {
+  console.log("I am in factorial");
+  let i = 0;
+  while (i < 200000000)
+    i++;
+
+  if (n < 0) // Error condition
+    return -1;
+  if (n === 0) // End of function
+    return 1;
+
+  return n * factorial(n - 1);
 }
 
 export default App;
