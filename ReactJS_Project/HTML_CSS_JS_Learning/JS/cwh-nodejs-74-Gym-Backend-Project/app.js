b07c077a4dffa607abab0247425cee9e5ff62933
Prashant Patel <prashant.patel@dixoninfo.com>
2023-10-11 14:52:22 +0530

    Added Gym project as backend support

--- ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-nodejs-74-Gym-Backend-Project/app.js
+++ ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-nodejs-74-Gym-Backend-Project/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 8080;
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'));
+app.use(express.urlencoded());
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug'); // Set the template engine as pug 
@@ -14,6 +16,21 @@ app.set('views', path.join(__dirname, 'views')); // Set the views directory
 app.get('/', (req, resp) => {
     const con = "This is best content available on the internet, therefore use wisely"
     const params = { 'title': 'Pubg is not good for kids', "content": con };
+    resp.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, resp) => {
+    // console.log(req.body);
+    name = req.body.name;
+    age = req.body.age;
+    gender = req.body.gender;
+    address = req.body.address;
+    more = req.body.more;
+
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${gender}, reside at ${address}. More about hi/her: ${more}`;
+    fs.writeFileSync('output.txt', outputToWrite);
+    
+    const params = { 'message': 'Your message is saved successfully' };
     resp.status(200).render('index.pug', params);
 })
 
