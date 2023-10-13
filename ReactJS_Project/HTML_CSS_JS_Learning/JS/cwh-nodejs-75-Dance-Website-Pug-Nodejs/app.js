8d689fd595284b7eb136bfe52663e901e017a16a
Prashant Patel <prashant.patel@dixoninfo.com>
2023-10-13 13:00:16 +0530

    Added intial webpage for Dance Academy

--- ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-nodejs-75-Dance-Website-Pug-Nodejs/app.js
+++ ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-nodejs-75-Dance-Website-Pug-Nodejs/app.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const express = require("express");
+const path = require("path");   
+const app = express();
+const port = 8080;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug'); // Set the template engine as pug 
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, resp) => {
+    const params = {};
+    resp.status(200).render('index.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+})
