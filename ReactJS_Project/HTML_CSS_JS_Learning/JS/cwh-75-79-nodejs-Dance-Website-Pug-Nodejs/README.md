c4c01c76168697087d44ad92c0453d37e21abc32
Prashant Patel <prashant.patel@dixoninfo.com>
2023-10-25 13:03:00 +0530

    Added database for contact page

--- ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-75-79-nodejs-Dance-Website-Pug-Nodejs/README.md
+++ ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-75-79-nodejs-Dance-Website-Pug-Nodejs/README.md
@@ -4,8 +4,11 @@
     `npm install express`
     `npm install nodemon`
     `npm install pug`
+    `npm install mongoose`
+    `npm install body-parser`
 
 ## Execute the commands
 `npm install` --> It will install all dependent packages
 `nodemon app` --> Run the Nodejs server
 `http://127.0.0.1:8080`
+`http://127.0.0.1:8080/contact`
--- ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-75-79-nodejs-Dance-Website-Pug-Nodejs/app.js
+++ ReactJS_Project/HTML_CSS_JS_Learning/JS/cwh-75-79-nodejs-Dance-Website-Pug-Nodejs/app.js
@@ -2,6 +2,22 @@ const express = require("express");
 const path = require("path");   
 const app = express();
 const port = 8080;
+const bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+
+// Connect to mongoose
+mongoose.connect('mongodb://127.0.0.1/contactDance', {useNewUrlParser: true});
+
+// Define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+var Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'));
@@ -22,6 +38,17 @@ app.get('/contact', (req, resp) => {
     const params = {};
     // resp.status(200).render('index.pug', params);
     resp.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, resp) => {
+    var myData = new Contact(req.body);
+    myData.save().then(() => {
+        resp.send("This item has been saved to database")
+    }).catch(() => {
+        resp.status(400).send("Item was not saved to the database");
+    });
+    console.log("Receive Parameter = ", myData);
+    // resp.status(200).render("contact.pug");
 })
 
 
