79eadce9d6f13acb4a8b11bdf3b369dffe09f90c
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-21 18:22:02 +0530

    Video-22-23 Added class base components

---
+++ ReactJS_Project/REACT_Learning/22-41-cwh-Project-02-NewsMonkey/newsmonkey/src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is news item component
+      </div>
+    )
+  }
+}
+
+export default NewsItem
