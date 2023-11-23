f886b15be435e5b154b18025294af50b0d955009
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-23 16:06:53 +0530

    Video-27 Using Fetch API in React to populate NewsItems

--- ReactJS_Project/REACT_Learning/22-41-cwh-Project-02-NewsMonkey/newsmonkey/src/components/News.js
+++ ReactJS_Project/REACT_Learning/22-41-cwh-Project-02-NewsMonkey/newsmonkey/src/components/News.js
@@ -79,6 +79,15 @@ export class News extends Component {
       loading: false,
     };
   }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9f8c2bef75eb45fa9d6c27cfe78a077e";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
     return (
       <div className="container my-3">
@@ -88,8 +97,10 @@ export class News extends Component {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title.slice(0, 45)}
-                  description={element.description.slice(0, 88)}
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
                   imgUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
--- ReactJS_Project/REACT_Learning/22-41-cwh-Project-02-NewsMonkey/newsmonkey/src/components/NewsItem.js
+++ ReactJS_Project/REACT_Learning/22-41-cwh-Project-02-NewsMonkey/newsmonkey/src/components/NewsItem.js
@@ -6,7 +6,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imgUrl} className="card-img-top" alt="..." />
+          <img
+            src={imgUrl ? imgUrl : require('./../images/noImg.png')}
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
