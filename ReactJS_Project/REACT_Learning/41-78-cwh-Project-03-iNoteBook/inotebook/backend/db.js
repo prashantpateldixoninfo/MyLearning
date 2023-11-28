8f75efc46c19febca755107674e02996aec111d3
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-28 12:03:26 +0530

    Video-46 Adding Data Validation Using express-validator

--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/db.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI).then(
--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = express.Router();
 
 // Create a User using: POST "/api/auth". Doesn't require Auth
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
+router.post("/", [body("name").isLength({ min: 3 }), body("email").isEmail(), body("password").isLength({ min: 5 })], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((user) => res.json(user))
+    .catch((err) => {
+      console.log(err);
+      res.json({ error: "Please enter a unique value for email", message: err.message });
+    });
 });
 
 module.exports = router;
