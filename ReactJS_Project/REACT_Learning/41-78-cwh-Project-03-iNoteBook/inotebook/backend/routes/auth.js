fb144a6fec72126309fa3fa9ab8fd4175122ad01
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-28 12:43:24 +0530

    Video-47 Creating ThunderClient Collections to Manage Requests

--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
@@ -3,22 +3,29 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = express.Router();
 
-// Create a User using: POST "/api/auth". Doesn't require Auth
-router.post("/", [body("name").isLength({ min: 3 }), body("email").isEmail(), body("password").isLength({ min: 5 })], (req, res) => {
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post("/createuser", [body("name").isLength({ min: 3 }), body("email").isEmail(), body("password").isLength({ min: 5 })], async (req, res) => {
+  // If there are errors, return Bad request and the errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then((user) => res.json(user))
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: "Please enter a unique value for email", message: err.message });
+  // Check whether the user with this email already exists
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" });
+    }
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
     });
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Some Error occured " + error.message);
+  }
 });
 
 module.exports = router;
