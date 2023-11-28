ab1ae206b23c92ee39ab777816c0b0e07b84b2da
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-28 15:54:11 +0530

    Video-50 Creating Login Endpoint & sending auth token

--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
@@ -34,10 +34,40 @@ router.post("/createuser", [body("name").isLength({ min: 3 }), body("email").isE
       },
     };
     const authToken = jwt.sign(data, JWT_SECRET);
-    res.json({authToken});
+    res.json({ authToken });
   } catch (error) {
     console.log(error.message);
-    res.status(500).send("Some Error occured " + error.message);
+    res.status(500).send("Internal Server Error " + error.message);
+  }
+});
+
+// Create a User using: POST "/api/auth/login".
+router.post("/login", [body("email", "Enter a valid email").isEmail(), body("password", "Password cannot be blank").exists()], async (req, res) => {
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({email});
+    if (!user) {
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
+    }
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({ authToken });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error " + error.message);
   }
 });
 
