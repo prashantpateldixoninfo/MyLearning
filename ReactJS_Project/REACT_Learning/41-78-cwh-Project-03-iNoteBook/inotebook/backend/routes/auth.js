241b8cc3aa547a7b2af1936f60e50b6867083180
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-28 15:21:16 +0530

    Video-49 Hashing Passwords using bcryptjs in NodeJs

--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Hello-JWT";
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post("/createuser", [body("name").isLength({ min: 3 }), body("email").isEmail(), body("password").isLength({ min: 5 })], async (req, res) => {
@@ -16,12 +20,21 @@ router.post("/createuser", [body("name").isLength({ min: 3 }), body("email").isE
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exists" });
     }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
     });
-    res.json(user);
+
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({authToken});
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Some Error occured " + error.message);
