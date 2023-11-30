659a671c7cff75ec87847619873e9bbfb6faefbc
Prashant Patel <prashant.patel@dixoninfo.com>
2023-11-30 12:52:30 +0530

    Video-53 Updating an existing Note

--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/notes.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/backend/routes/notes.js
@@ -39,4 +39,32 @@ router.post("/addnote", fetchuser, [body("title", "Enter a valid title").isLengt
   }
 });
 
+// ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote". Login required.
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  // Create a newNode object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  // Find the note to be updated and update it
+  let  note = await Note.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found!");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed!");
+  }
+  note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+  res.json(note);
+});
+
 module.exports = router;
