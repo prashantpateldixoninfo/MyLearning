522c6eecd70b1e4dca70e2f572402bafa9b88456
Prashant Patel <prashant.patel@dixoninfo.com>
2023-12-01 18:19:10 +0530

    Video-58 useContext hook: Using React Context API

--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/src/App.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/src/App.js
@@ -2,18 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home key="inotebook" />} />
-          <Route exact path="/home" element={<Home key="home" />} />
-          <Route exact path="/about" element={<About key="about" />} />
-        </Routes>
-      </BrowserRouter>
+      <NoteState>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home key="inotebook" />} />
+            <Route exact path="/home" element={<Home key="home" />} />
+            <Route exact path="/about" element={<About key="about" />} />
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
     </div>
   );
 }
--- ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/src/components/About.js
+++ ReactJS_Project/REACT_Learning/41-78-cwh-Project-03-iNoteBook/inotebook/src/components/About.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
 
 export default function About() {
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
   return (
     <div>
-      <h1>This is About</h1>
+      <h1>
+        This is About {a.state.name} and he is in class {a.state.class}
+      </h1>
     </div>
   );
 }
