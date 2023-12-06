831f542051220436e74bdaa44f797cea84553aaf
Prashant Patel <prashant.patel@dixoninfo.com>
2023-12-06 17:22:41 +0530

    Video-76 Creating a Redux Store

--- ReactJS_Project/REACT_Learning/73-78-cwh-Redux-Application/reduxapp/src/state/reducers/amountReducer.js
+++ ReactJS_Project/REACT_Learning/73-78-cwh-Redux-Application/reduxapp/src/state/reducers/amountReducer.js
@@ -1,4 +1,4 @@
-export default reducer = (state = 0, action) => {
+const reducer = (state = 0, action) => {
   if (action === "deposit") {
     return state + action.payload;
   } else if (action === "withdraw") {
@@ -7,3 +7,5 @@ export default reducer = (state = 0, action) => {
     return state;
   }
 };
+
+export default reducer;
