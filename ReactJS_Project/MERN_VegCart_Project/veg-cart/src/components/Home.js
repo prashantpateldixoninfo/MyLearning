935e832edf3bc6f9807266b45baa341f4f96db32
Prashant Patel <prashant.patel@dixoninfo.com>
2023-12-26 18:31:38 +0530

    Added home page image

--- ReactJS_Project/MERN_VegCart_Project/veg-cart/src/components/Home.js
+++ ReactJS_Project/MERN_VegCart_Project/veg-cart/src/components/Home.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function Home() {
     return (
         <div>
-            <h1>This is home page</h1>
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
+            <img src="https://source.unsplash.com/random/900%C3%97700/?vegetable" style={{ width: "250px", height: "250px", border: "2px solid white" }} alt="Vegetables" />
         </div>
     );
 }
