6d8c3a42b1cd6dc15d3f2a7a1f73e030e1f0b31c
Prashant Patel <prashant.patel@dixoninfo.com>
2024-01-13 18:12:22 +0530

    Video-62 JavaScript Exercise 10 - Business Name Generator

---
+++ ReactJS_Learning_Projects/CWH_Learning_Projects/CWH_SIGMA_WEB_DEV/Video-62-JS-Exercise-10-Business-Name-Generator/index.js
@@ -0,0 +1,28 @@
+/* Create a business name generator by combining list of adjectives and shop name and another word
+
+
+Adjectives:
+Crazy 
+Amazing
+Fire 
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+
+
+*/
+
+let adjectives = ["Crazy", "Amazing", "Fire"];
+let shopName = ["Engine", "Foods", "Garments"];
+let anotherWord = ["Bros", "Limited", "Hub"];
+
+let businessName = adjectives[Math.floor(Math.random() * 100) % 3] + shopName[Math.floor(Math.random() * 100) % 3] + anotherWord[Math.floor(Math.random() * 100) % 3];
+console.log(businessName);
