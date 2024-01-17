d964360a9a632ca354e44069841655858b4c7ded
Prashant Patel <prashant.patel@dixoninfo.com>
2024-01-17 17:13:47 +0530

    Video-67 JavaScript DOM - Children, Parent & Sibling Nodes

---
+++ ReactJS_Learning_Projects/CWH_Learning_Projects/CWH_SIGMA_WEB_DEV/Video-67-DOM-Children-Parent-Siblings/script.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+
+console.log(document.body.parentNode);
+console.log(document.body.parentElement);
+console.log(document.body.childNodes);
+console.log(document.body.firstElementChild);
+console.log(document.body.firstElementChild.childNodes);
+console.log(document.body.firstElementChild.children);
+document.body.firstElementChild.children[3].style.backgroundColor = "orange";
